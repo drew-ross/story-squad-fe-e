b59e9c4ac22e9e2288306d81455c877b7bdaafe4
Drew Ross <mail.drew.ross@gmail.com>
2020-11-05 14:22:22 -0800

    Adds Emoji component and functions to add/remove emojis from feedback list. Renders emojis to select/remove.

--- src/components/common/EmojiPicker.js
+++ src/components/common/EmojiPicker.js
@@ -83,11 +83,43 @@ const emojiList = [
   '😤',
 ];
 
+const Emoji = props => {
+  const { emoji, handleClick } = props;
+
+  return (
+    <button className="emoji" onClick={() => handleClick(emoji)}>
+      {emoji}
+    </button>
+  );
+};
+
 const EmojiPicker = props => {
+  const [selectedEmojis, setSelectedEmojis] = useState([]);
+
+  const handleAddEmoji = emoji => {
+    if (selectedEmojis.length < 6 && !selectedEmojis.includes(emoji)) {
+      setSelectedEmojis([...selectedEmojis, emoji]);
+    }
+  };
+
+  const handleRemoveEmoji = emoji => {
+    setSelectedEmojis(
+      selectedEmojis.filter(selectedEmoji => selectedEmoji !== emoji)
+    );
+  };
+
   return (
     <>
-      <Card title="Your Feedback">Emojis go here</Card>
-      <Card title="Give Feedback">{emojiList.toString()}</Card>
+      <Card title="Your Feedback">
+        {selectedEmojis.map(emoji => (
+          <Emoji emoji={emoji} handleClick={handleRemoveEmoji} />
+        ))}
+      </Card>
+      <Card title="Give Feedback">
+        {emojiList.map(emoji => (
+          <Emoji emoji={emoji} handleClick={handleAddEmoji} />
+        ))}
+      </Card>
     </>
   );
 };
